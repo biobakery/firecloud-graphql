c893b5831d7ebb5d660b433ee674e8503c5f559a
ljmciver <lauren.j.mciver@gmail.com>
2018-12-05 00:55:34 +0000

    Add more notes on reference

--- server.py
+++ server.py
@@ -2,8 +2,10 @@
 # Basic server to host graphql with flask on localhost:5000
 # Interfaces to firecloud api
 
-# The initial server was generated using the example code from
-# the REST wrapper from graphql example at https://github.com/nderkach/python-grahql-api
+# The initial server was based on the core framework from the example code from
+# https://github.com/nderkach/python-grahql-api
+# which wraps an end point of the RESTful AirBnB API with GraphQl.
+# Code from the example is included in the utilities functions.
 
 import os
 
