c52f40bf62ad5b054fdb0615c98c049d657c9506
ljmciver <lauren.j.mciver@gmail.com>
2019-02-21 20:41:05 +0000

    Organize schema

--- schema.py
+++ schema.py
@@ -87,21 +87,6 @@ class Project(graphene.ObjectType):
     def get_node(cls, info, id):
         return get_project(id)
 
-CURRENT_PROGRAMS = [Program(name="NHSII")]
-
-DATA_CATEGORIES = [DataCategories(case_count=5, data_category="Raw Reads"),
-                   DataCategories(case_count=5, data_category="Gene Families"),
-                   DataCategories(case_count=5, data_category="Taxonomic Profiles")]
-
-CURRENT_PROJECTS = {
-    "1":Project(id="1", project_id="NHSII-DemoA", name="NHSII-DemoA", program=CURRENT_PROGRAMS[0], summary=Summary(case_count=5, file_count=15, data_categories=DATA_CATEGORIES), primary_site=["Stool"]),
-    "2":Project(id="2", project_id="NHSII-DemoB", name="NHSII-DemoB", program=CURRENT_PROGRAMS[0], summary=Summary(case_count=5, file_count=15, data_categories=DATA_CATEGORIES), primary_site=["Stool"]),
-    "3":Project(id="3", project_id="NHSII-DemoC", name="NHSII-DemoC", program=CURRENT_PROGRAMS[0], summary=Summary(case_count=5, file_count=15, data_categories=DATA_CATEGORIES), primary_site=["Stool"]),
-} 
-
-def get_project(id):
-    return CURRENT_PROJECTS[id]
-
 class FileCase(graphene.ObjectType):
     class Meta:
         interfaces = (graphene.relay.Node,)
@@ -117,16 +102,6 @@ class FileCaseConnection(graphene.relay.Connection):
 
     def resolve_total(self, info):
         return 1
-
-CURRENT_FILE_CASES = {
-    "1":FileCase(1,"Case1",get_project("1")),
-    "2":FileCase(2,"Case2",get_project("1")),
-    "3":FileCase(3,"Case3",get_project("2")),
-    "4":FileCase(4,"Case4",get_project("3")),
-}
-
-def get_filecase(id):
-    return CURRENT_FILE_CASES[id]
 
 class FileCases(graphene.ObjectType):
     hits = graphene.relay.ConnectionField(FileCaseConnection,
@@ -169,25 +144,6 @@ class File(graphene.ObjectType):
     def get_node(cls, info, id):
         return get_file(id)
 
-def get_file(id):
-    return TEST_FILES[id]
-
-TEST_FILES = {
-    "1": File(1, "demo_A1.fastq","person1","sample1","fastq", "raw", "open", 50, "Raw Reads", "Illumina", "Fastq", "WMGX", FileCases(hits=["1"])),
-    "2": File(2, "demo_A2.fastq","person2","sample2","fastq", "raw", "open", 50, "Raw Reads", "Illumina", "Fastq", "WMGX", FileCases(hits=["2"])),
-    "3": File(3, "demo_B1.fastq","person1B","sample1B","fastq", "raw", "open", 50, "Raw Reads", "Illumina", "Fastq", "WMGX", FileCases(hits=["3"])),
-    "4": File(4, "demo_B2.fastq","person2B","sample2B","fastq", "raw", "open", 50, "Raw Reads", "Illumina", "Fastq", "WMGX", FileCases(hits=["4"]))
-}
-
-CURRENT_COUNTS = Count(
-    projects="3",
-    participants="15",
-    samples="15",
-    dataFormats="3",
-    rawFiles="15",
-    processedFiles="30"
-)
-
 class ProjectConnection(graphene.relay.Connection):
     class Meta:
         node = Project
@@ -205,7 +161,7 @@ class FileConnection(graphene.relay.Connection):
         return get_total_files()
 
 class Files(graphene.ObjectType):
-    hits = graphene.relay.ConnectionField(FileConnection, 
+    hits = graphene.relay.ConnectionField(FileConnection,
         first=graphene.Int(),
         offset=graphene.Int(),
         sort=graphene.List(Sort),
@@ -214,16 +170,11 @@ class Files(graphene.ObjectType):
     def resolve_hits(self, info, first=None, offset=None, sort=None, filters=None):
         return [get_file(file_id) for file_id in self.hits]
 
-CURRENT_FILES = Files(hits=["1","2","3","4"])
-
-def get_total_files():
-    return len(CURRENT_FILES.hits)
-
 class Repository(graphene.ObjectType):
     files = graphene.Field(Files)
 
     def resolve_files(self, info):
-        return CURRENT_FILES
+        return get_current_files()
 
 class Bucket(graphene.ObjectType):
     doc_count = graphene.Int()
@@ -236,38 +187,12 @@ class Bucket(graphene.ObjectType):
 class Aggregations(graphene.ObjectType):
     buckets = graphene.List(Bucket)
 
-
-PROJECT_AGGREGATIONS={
-    "primary_site": Aggregations(buckets=[Bucket(doc_count=45, key="Stool")]),
-    "program__name": Aggregations(buckets=[Bucket(doc_count=45, key="NHSII")]),
-    "project_id": Aggregations(buckets=[Bucket(doc_count=15, key="NHSII-DemoA"),
-                   Bucket(doc_count=15, key="NHSII-DemoB"),
-                   Bucket(doc_count=15, key="NHSII-DemoC")]),
-    "summary__data_categories__data_category": Aggregations(buckets=[Bucket(doc_count=15, key="Raw Reads"),
-                                                Bucket(doc_count=15, key="Gene Families"),
-                                                Bucket(doc_count=15, key="Taxonomic Profiles")]),
-    "summary__experimental_strategies__experimental_strategy": Aggregations(buckets=[Bucket(doc_count=30, key="WMGX"),
-                                                                Bucket(doc_count=15, key="16S")]),
-}
-
-
 class ProjectAggregations(graphene.ObjectType):
     primary_site = graphene.Field(Aggregations)
     program__name = graphene.Field(Aggregations)
     project_id = graphene.Field(Aggregations)
     summary__experimental_strategies__experimental_strategy = graphene.Field(Aggregations)
     summary__data_categories__data_category = graphene.Field(Aggregations)
-
-    def resolve_primary_site(self, info):
-       return PROJECT_AGGREGATIONS["primary_site"]
-    def resolve_program__name(self, info):
-       return PROJECT_AGGREGATIONS["program__name"]
-    def resolve_project_id(self, info):
-       return PROJECT_AGGREGATIONS["project_id"]
-    def resolve_summary__experimental_strategies__experimental_strategy(self, info):
-       return PROJECT_AGGREGATIONS["summary__experimental_strategies__experimental_strategy"]
-    def resolve_summary__data_categories__data_category(self, info):
-       return PROJECT_AGGREGATIONS["summary__data_categories__data_category"]
 
 class Projects(graphene.ObjectType):
     aggregations = graphene.Field(ProjectAggregations, 
@@ -283,7 +208,7 @@ class Projects(graphene.ObjectType):
         return [get_project(project_id) for project_id in CURRENT_PROJECTS.keys()]
 
     def resolve_aggregations(self, info, filters=None, aggregations_filter_themselves=None):
-        return ProjectAggregations(self)
+        return get_project_aggregations()
 
 class Root(graphene.ObjectType):
     user = graphene.Field(User)
@@ -329,3 +254,71 @@ class Query(graphene.ObjectType):
         obj_result = utilities.json2obj(json.dumps(json_result))
         return obj_result
      
+# Constants
+def get_filecase(id):
+    return CURRENT_FILE_CASES[id]
+
+def get_project(id):
+    return CURRENT_PROJECTS[id]
+
+def get_file(id):
+    return TEST_FILES[id]
+
+def get_total_files():
+    return len(CURRENT_FILES.hits)
+
+def get_current_files():
+    return CURRENT_FILES
+
+def get_project_aggregations():
+    return PROJECT_AGGREGATIONS
+
+CURRENT_PROGRAMS = [Program(name="NHSII")]
+
+DATA_CATEGORIES = [DataCategories(case_count=5, data_category="Raw Reads"),
+                   DataCategories(case_count=5, data_category="Gene Families"),
+                   DataCategories(case_count=5, data_category="Taxonomic Profiles")]
+
+CURRENT_PROJECTS = {
+    "1":Project(id="1", project_id="NHSII-DemoA", name="NHSII-DemoA", program=CURRENT_PROGRAMS[0], summary=Summary(case_count=5, file_count=15, data_categories=DATA_CATEGORIES), primary_site=["Stool"]),
+    "2":Project(id="2", project_id="NHSII-DemoB", name="NHSII-DemoB", program=CURRENT_PROGRAMS[0], summary=Summary(case_count=5, file_count=15, data_categories=DATA_CATEGORIES), primary_site=["Stool"]),
+    "3":Project(id="3", project_id="NHSII-DemoC", name="NHSII-DemoC", program=CURRENT_PROGRAMS[0], summary=Summary(case_count=5, file_count=15, data_categories=DATA_CATEGORIES), primary_site=["Stool"]),
+} 
+
+CURRENT_FILE_CASES = {
+    "1":FileCase(1,"Case1",get_project("1")),
+    "2":FileCase(2,"Case2",get_project("1")),
+    "3":FileCase(3,"Case3",get_project("2")),
+    "4":FileCase(4,"Case4",get_project("3")),
+}
+
+TEST_FILES = {
+    "1": File(1, "demo_A1.fastq","person1","sample1","fastq", "raw", "open", 50, "Raw Reads", "Illumina", "Fastq", "WMGX", FileCases(hits=["1"])),
+    "2": File(2, "demo_A2.fastq","person2","sample2","fastq", "raw", "open", 50, "Raw Reads", "Illumina", "Fastq", "WMGX", FileCases(hits=["2"])),
+    "3": File(3, "demo_B1.fastq","person1B","sample1B","fastq", "raw", "open", 50, "Raw Reads", "Illumina", "Fastq", "WMGX", FileCases(hits=["3"])),
+    "4": File(4, "demo_B2.fastq","person2B","sample2B","fastq", "raw", "open", 50, "Raw Reads", "Illumina", "Fastq", "WMGX", FileCases(hits=["4"]))
+}
+
+CURRENT_COUNTS = Count(
+    projects="3",
+    participants="15",
+    samples="15",
+    dataFormats="3",
+    rawFiles="15",
+    processedFiles="30"
+)
+
+CURRENT_FILES = Files(hits=["1","2","3","4"])
+
+PROJECT_AGGREGATIONS=ProjectAggregations(
+    primary_site=Aggregations(buckets=[Bucket(doc_count=45, key="Stool")]),
+    program__name=Aggregations(buckets=[Bucket(doc_count=45, key="NHSII")]),
+    project_id=Aggregations(buckets=[Bucket(doc_count=15, key="NHSII-DemoA"),
+                   Bucket(doc_count=15, key="NHSII-DemoB"),
+                   Bucket(doc_count=15, key="NHSII-DemoC")]),
+    summary__data_categories__data_category=Aggregations(buckets=[Bucket(doc_count=15, key="Raw Reads"),
+                                                Bucket(doc_count=15, key="Gene Families"),
+                                                Bucket(doc_count=15, key="Taxonomic Profiles")]),
+    summary__experimental_strategies__experimental_strategy=Aggregations(buckets=[Bucket(doc_count=30, key="WMGX"),
+                                                                Bucket(doc_count=15, key="16S")]))
+
