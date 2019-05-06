64a7711199b72c1aec6d06d253f47eff4aec9dca
ljmciver <lauren.j.mciver@gmail.com>
2019-05-06 17:00:50 +0000

    Remove firecloud api calls from schema, as no longer in use, originally added as part of the demo server

--- schema.py
+++ schema.py
@@ -29,20 +29,6 @@ def filter_noauth(data):
     except KeyError:
         pass
 
-## Firecloud API ##
-
-class entitiesWithType(graphene.ObjectType):
-    namespace = graphene.ID()
-    workspace = graphene.ID()
-    attributes = graphene.String()
-
-class entity(graphene.ObjectType):
-    namespace = graphene.ID()
-    workspace = graphene.ID()
-    name = graphene.String()
-    entityType = graphene.String()
-    attributes = graphene.String()
-
 ## Portal API ##
 
 class Sort(graphene.String):
@@ -548,22 +534,4 @@ class Query(graphene.ObjectType):
 
     def resolve_viewer(self, info):
         return Root(self,info)
-
-    ## Firecloud API ##
-    entities_with_type = graphene.List(entitiesWithType, namespace=graphene.ID(required=True), workspace=graphene.ID(required=True))
-
-    samples = graphene.List(entity, namespace=graphene.ID(required=True), workspace=graphene.ID(required=True))
-
-    def resolve_entities_with_type(self, info, namespace, workspace):
-        url = "workspaces/{0}/{1}/entities_with_type".format(namespace, workspace)
-        json_result = query_firecloud.call_api(url)
-        obj_result = utilities.json2obj(json.dumps(json_result))
-        return obj_result
-
-    def resolve_samples(self, info, namespace, workspace):
-        url = "workspaces/{0}/{1}/entities/{type}".format(namespace, workspace, "sample")
-        json_result = query_firecloud.call_api(url)
-        obj_result = utilities.json2obj(json.dumps(json_result))
-        return obj_result
-     
 
